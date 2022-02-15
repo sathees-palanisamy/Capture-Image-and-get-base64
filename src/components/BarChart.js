5d189ef17367469afd603a63732de517401b3f78
sathees-palanisamy <dharsansathees@gmail.com>
2022-02-15 12:42:10 +0530

    second commit

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,20 +1,34 @@
-import React, { useEffect, createRef } from "react";
-import { Bar } from "react-chartjs-2";
+import React, { useEffect, createRef, useRef, useState } from "react";
+import { Bar, getDatasetAtEvent } from "react-chartjs-2";
 import { useScreenshot } from "use-react-screenshot";
 
 function BarChart({ chartData }) {
+
+  const chartRef = useRef();
   const imageRef = createRef(null);
   const [image, takeScreenShot] = useScreenshot();
+  const [datasets,setDatasets] = useState(null);
 
   const getImage = () => {
+    console.log('chartRef:' + chartRef);
     return takeScreenShot(imageRef.current);
   };
 
   useEffect(() => {
     if (image) {
-      console.log("image:" + image);
+      // console.log("image:" + image);
+      const dataToServer = {
+        imageBlob: image,
+        chartDatasets: datasets
+      }
     }
   }, [image]);
+
+
+  const onClick = (event) => {
+    console.log(getDatasetAtEvent(chartRef.current, event));
+    setDatasets(getDatasetAtEvent(chartRef.current, event))
+  }
 
   return (
     <div>
@@ -28,7 +42,7 @@ function BarChart({ chartData }) {
           marginTop: "20px",
         }}
       >
-        <Bar data={chartData} />
+        <Bar data={chartData}  ref={chartRef} onClick={onClick} />
       </div>
     </div>
   );
